32d1df03d3bfe2b7c8551a61ee20c2cd93ba21ee
thaianh4780 <100453973+thaianh4780@users.noreply.github.com>
2022-12-27 14:39:25 +0700

    done setup v1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Test from './Test';
-import "../css/bootstrap.min.css"
+import Test from '../pages/Test';
+import "../css2/bootstrap.min.css"
 import About from './About';
 import MoreProduct from './MoreProduct';
 import PageSection from './PageSection';
 const Body = () => {
     return (
         <div
-            style={{ backgroundImage: `url("assets/img/bg.jpg")` ,marginTop:0 }}>
+            style={{ backgroundImage: `url("assets/img/bg.jpg")` ,marginTop:"-5%" }}>
             <h1 class="text-center text-white d-none d-lg-block site-heading pt-5">
                 <span class="site-heading-lower">Business Casual</span>
             </h1>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Body from "../components/Body";
-
 export default function Home() {
   return (
     <>
