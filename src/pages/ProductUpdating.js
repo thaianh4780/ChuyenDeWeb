517c1ee5ae313d81d57b3e6290a468ca6018b4eb
phuong1312 <20211tt1151@mail.tdc.edu.vn>
2022-12-31 13:45:37 +0700

    ok

--- src/pages/ProductUpdating.js
+++ src/pages/ProductUpdating.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Cookies from 'universal-cookie/cjs/Cookies'
+import url from '../Url'
 import AdminFooter from '../components/AdminFooter'
 import AdminNav from '../components/AdminNav'
 import AdminStaff from '../components/AdminStaff'
@@ -7,6 +10,24 @@ import ProductForm from '../components/ProductForm'
 import '../css1/bootstrap.min.css'
 
 export default function ProductUpdating() {
+
+    const navigate = useNavigate()
+    const cookie = new Cookies();
+    useEffect(()=>{
+        if (!cookie.get('token')) {
+            return navigate('../login', { replace: true })
+          } else {
+             fetch(url + 'user/check/' + cookie.get('token'))
+              .then((res) => res.json())
+              .then((res) => {
+                if (res.error) {
+                  console.log(res.error);
+                  return navigate('../login', { replace: true })
+                }
+              })
+              .catch((err) => console.log("ERR", err));
+          }  
+    }, [])
     return (
         <div id="page-top">
             <div id="wrapper">
